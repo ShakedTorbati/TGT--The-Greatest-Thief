df2477d86ca5954e47ac103fa866024f69fbf3d9
ShakedTorbati <shakedtorb@gmail.com>
2024-05-31 15:38:08 +0300

    changes

--- bedroom-1/bedroom.js
+++ bedroom-1/bedroom.js
@@ -1,4 +1,6 @@
 function note(){
+    alert("hjhcj");
     var note= document.getElementById("note");
-    note.style.width="100px";
+    note.style.width="500px";
+    note.style.height="500px";
 }
