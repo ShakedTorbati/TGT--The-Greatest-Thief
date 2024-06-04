a83042baeb0e38e0366178a1d664589e85fc3e4a
shellyraz6 <shellyraz6@gmail.com>
2024-06-04 20:46:03 +0300

    homepage

---
+++ homepage/homepage.js
@@ -0,0 +1,23 @@
+function gotobedroom(){
+    window.location.href="../bedroom-1/bedroom.html"
+}
+
+function gotocharacters(){
+    window.location.href="../wall-2/wall.html"
+}
+
+function gotoclothest(){
+    window.location.href="../living room-3/living_room.html"
+}
+
+function gotosalon(){
+    window.location.href="..//wall.html"
+}
+
+function gotocharacters(){
+    window.location.href="../wall-2/wall.html"
+}
+
+function gotocharacters(){
+    window.location.href="../wall-2/wall.html"
+}
