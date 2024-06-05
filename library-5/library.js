facaa83b8fe13ddc34bd8c59e61d9a411005b97e
ShakedTorbati <shakedtorb@gmail.com>
2024-06-05 17:58:07 +0300

    Vfjnf

---
+++ library-5/library.js
@@ -0,0 +1,14 @@
+function books1(){
+    alert("Nop, wrong one1")
+}
+function books2(){
+    alert("Nop, wrong one2")
+}
+function books3(){
+    alert("hi")
+    var open_book=document.getElementById("open_book");
+    open_book.style.visibility="visible";
+}
+function books4(){
+    alert("Nop, wrong one4");
+}
