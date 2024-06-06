9a287806edaf5f4b7df5c1cb956ba0f315d40557
ShakedTorbati <shakedtorb@gmail.com>
2024-06-06 18:11:35 +0300

    fnfbj

--- living room-3/living_room.js
+++ living room-3/living_room.js
@@ -1,9 +1,6 @@
 function homepage(){
     window.location.href = "../homepage/homepage.html";
 }
-function movetoletter(){
-    letter=document.getElementById("picture")
-    letter.style.width="1000px";
-    letter.style.height="500px";
-    alert("kfjhkfakf")
+function solve(){
+    prompt("What is your answer?");
 }
