1c754dd5540f89b5b461b552b0baca49a010bb42
ShakedTorbati <shakedtorb@gmail.com>
2024-06-06 17:16:47 +0300

    endd

--- workroom-6/workroom.js
+++ workroom-6/workroom.js
@@ -1,9 +1,16 @@
 function poppic(){
-    woodbord2=document.getElementById("picture")
+    var woodbord2=document.getElementById("picture")
     woodbord2.style.width="500px";
     woodbord2.style.height="500px";
-    alert("kfjhkfakf")
+    var board_container=document.getElementById("board-container")
+    board_container.style.display="block";
 }
 function homepage(){
     window.location.href = "../homepage/homepage.html";
+}
+function right(){
+    alert("You are right, She was the thief!you are a good inspector!")
+}
+function wrong(){
+    alert("Nope!you are a bad inspector!");
 }
