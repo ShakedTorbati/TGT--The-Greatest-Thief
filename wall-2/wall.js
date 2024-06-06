bd69292bc1cb4f5d71bc1e63b4f0c0c1ffb0c18e
ShakedTorbati <shakedtorb@gmail.com>
2024-06-06 16:14:31 +0300

    wall

--- wall-2/wall.js
+++ wall-2/wall.js
@@ -1,3 +1,17 @@
-function homepage(){
+function homepage() {
     window.location.href = "../homepage/homepage.html";
+}
+function bell() {
+    var info = document.getElementById("info").style.visibility="visible";
+    var info_p = document.getElementById("info_p");
+    info_p.innerHTML="Bell Montgomery, a 32-year-old resident of New Orleans, is under suspicion for the recent theft at Oakhaven Manor. Known for her quick wit and dexterous hands, Bell works as a professional magician, often captivating audiences with her sleight of hand and illusion tricks. However, her talents have also earned her a reputation for being able to acquire objects without detection, leading investigators to believe she could be the mastermind behind the heist.";
+}
+function closeInfo() {
+    var info = document.getElementById("info");
+    info.style.display = "none";
+}
+function () {
+    var info = document.getElementById("info").style.visibility="visible";
+    var info_p = document.getElementById("info_p");
+    info_p.innerHTML="Bell Montgomery, a 32-year-old resident of New Orleans, is under suspicion for the recent theft at Oakhaven Manor. Known for her quick wit and dexterous hands, Bell works as a professional magician, often captivating audiences with her sleight of hand and illusion tricks. However, her talents have also earned her a reputation for being able to acquire objects without detection, leading investigators to believe she could be the mastermind behind the heist.";
 }
