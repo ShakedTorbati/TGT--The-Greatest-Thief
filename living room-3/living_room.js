46a687a94dd82c0ccbfa5f21531d36b07ceeeff7
shellyraz6 <shellyraz6@gmail.com>
2024-06-06 17:30:20 +0300

    gw

--- living room-3/living_room.js
+++ living room-3/living_room.js
@@ -1,3 +1,9 @@
 function homepage(){
     window.location.href = "../homepage/homepage.html";
 }
+function movetoletter(){
+    picture=document.getElementById("picture")
+    picture.style.width="1000px";
+    picture.style.height="500px";
+    alert("kfjhkfakf")
+}
